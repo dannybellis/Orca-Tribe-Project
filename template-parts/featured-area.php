4f9f02d8f887580743ccfbfdef8f3d84ff2975ea
d-h-231 <dxh.diane.huang@gmail.com>
2017-05-19 21:54:19 -0500

    resize works
    ...but need to set conditionals for vertical or horizontal images? consider jquery option and just process images ahead of time.

--- template-parts/featured-area.php
+++ template-parts/featured-area.php
@@ -13,7 +13,7 @@ $ftstory_query = new WP_Query( array(
             $fts_postID = $post->ID;
             $fts_ID = get_the_id();
             $fts_headline = get_the_title($fts_postID);
-            $fts_date = get_the_date($fts_postID);
+
             $fts_featured_image = get_the_post_thumbnail($fts_postID);
             $fts_URL = get_the_permalink($fts_postID);
         endwhile;
@@ -26,7 +26,7 @@ $ftartist_query = new WP_Query( array(
             $fta_postID = $post->ID;
             $fta_ID = get_the_id();
             $fta_headline = get_the_title($fta_postID);
-            $fta_date = get_the_date($fta_postID);
+
             $fta_featured_image = get_the_post_thumbnail($fta_postID);
             $fta_URL = get_the_permalink($fta_postID);
         endwhile;
@@ -56,18 +56,28 @@ $ftartist_query = new WP_Query( array(
         left: 16px;
     }
     
+    .featuredtopright {
+        position: absolute;
+        top: 8px;
+        right: 16px;
+        text-align: right;
+    }
+    
     .featured img {
-        clip: rect(0px 385px 385px 0px);
+        display:block;
+        max-height: 385px;
+        width: auto;
+        height: 385px;
     }
 </style>
 <!--Featured Story-->
+<?php echo $before_widget?>
 <a href="<?php echo $fts_URL;?>">
     <div class="featured-story">
         <div class="featured">
         <?php echo $fts_featured_image;?>
             <div class="featuredbottomleft">
                 <h1><?php echo $fts_headline;?></h1>
-                <p><?php echo $fts_date; ?> </p>
             </div>
         </div>
     </div>
@@ -77,9 +87,8 @@ $ftartist_query = new WP_Query( array(
     <div class="featured-artist">
         <div class="featured">
         <?php echo $fta_featured_image;?>
-            <div class="featuredbottomleft">
+            <div class="featuredtopright">
                 <h1><?php echo $fta_headline;?></h1>
-                <p><?php echo $fta_date; ?> </p>
             </div>
         </div>
     </div>
